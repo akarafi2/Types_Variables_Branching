9a4cc3378196a1d9030b2e4c3a3ff3e42094e92c
Adam Karafi <akarafi@gmail.com>
2022-06-23 00:53:52 -0500

    added gitignore and files

---
+++ src/Types_Variables_Branching.java
@@ -0,0 +1,16 @@
+
+public class Types_Variables_Branching {
+	public static void main(String[] args) {
+		System.out.println("This is working!");
+		String 3stars = "Three Stars";
+		int I-am-a-variable = 3;
+		long #Long = 26000;
+		float float = 26.5;
+		int uninitialized;
+		Strimg word;
+		
+		System.out.print(uninitialized);
+		System.out.print(word);
+	
+	}
+}
